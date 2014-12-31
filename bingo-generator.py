0ab4dfbb7d7aa12dd970303f1aafad3e723be365
Aaron Fields <spirotot@gmail.com>
2014-12-31 18:27:44 -0500

    Modified this program so that it:
    * Prints a "BINGO" header on each card.
    * Prints 4 cards to a page.
    * Uses randomly generated numbers to populate the cards, instead of a text file.
    * Generates no duplicate numbers in rows/columns, and no duplicate cards.

--- bingo-generator.py
+++ bingo-generator.py
@@ -3,16 +3,23 @@
 import random, sys
 
 # check for the right # of args
-if len(sys.argv) != 4:
-    print "USAGE: " + sys.argv[0], " [file of terms] [output file] [# of cards]"
-    print "Example: " + sys.argv[0] + " bingo_terms.txt bingo.html 20"
+if len(sys.argv) != 3:
+    print "USAGE: " + sys.argv[0], " [output file] [# of cards]"
+    print "Example: " + sys.argv[0] + " bingo.html 20"
     sys.exit(1)
 
 # read in the bingo terms
-in_file = open(sys.argv[1], 'r')
-terms = [line.strip() for line in in_file.readlines()]
-terms = filter(lambda x: x != "", terms)
-in_file.close()
+
+pull_div = "left"
+
+def left_or_right():
+    global pull_div
+    if pull_div == "left":
+        pull_div = "right"
+    else:
+        pull_div = "left"
+
+    return pull_div
 
 # XHTML4 Strict, y'all!
 head = ("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n"
@@ -20,20 +27,89 @@ head = ("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org
         "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n"
         "<title>Bingo Cards</title>\n"
         "<style type=\"text/css\">\n"
-        "\tbody { font-size: 12px; }\n"
-        "\ttable { margin: 40px auto; border-spacing: 2px; }\n"
+        "\tbody { font-size: 10px; }\n"
+        "\ttable { margin: 20px auto; border-spacing: 2px; }\n"
         "\t.newpage { page-break-after:always; }\n"
         "\ttr { height: 60px; }\n"
-        "\ttd { text-align: center; border: thin black solid; padding: 10px; width: 60px; }\n"
+        "\ttd { text-align: center; border: thin black solid; padding: 5px; width: 60px; }\n"
         "</style>\n</head>\n<body>\n")
 
-# Generates an HTML table representation of the bingo card for terms
-def generateTable(terms, pagebreak = True):
-    ts = terms[:12] + ["FREE SPACE"] + terms[12:24]
-    if pagebreak:
-        res = "<table class=\"newpage\">\n"
+def getRandom(low, high, used):
+    i = 0
+    while i in used:
+        i = random.randint(low, high)
+
+    used.append(i)
+
+    return str(i)
+
+used_tables = list()
+
+def check_unique_table(table):
+    global used_tables
+    
+    if len(used_tables) > 1:
+        for used_table in used_tables:
+            for i in range(0, 25):
+                if used_table[i] != table[i]:
+                    used_tables.append(table)
+                    return True
+
     else:
-        res = "<table>\n"
+        used_tables.append(table)
+        return True
+
+    return False
+
+def generateNumbers():
+    used = list()
+    used.append(0)
+
+    while True:
+        l = list()
+        for i in range(0, 5):
+            l.append([None] * 5)
+
+        for row in range(0, 5):
+            for column in range(0, 5):
+                if column == 0:
+                    l[row][column] = getRandom(1, 15, used)
+                if column == 1:
+                    l[row][column] = getRandom(16, 30, used) #random.randint(16, 30)
+                if column == 2:
+                    l[row][column] = getRandom(31, 45, used) #random.randint(31, 45)
+                if column == 3:
+                    l[row][column] = getRandom(46, 60, used) #random.randint(46, 60)
+                if column == 4:
+                    l[row][column] = getRandom(61, 75, used) #random.randint(61, 75)
+
+
+        l2 = list()
+        for row in range(0, 5):
+            for column in range(0, 5):
+                l2.append(l[row][column])
+
+        if check_unique_table(l2):
+            return l2
+        else:
+            print "Wow, found a duplicate table!"
+            continue
+
+# Generates an HTML table representation of the bingo card for terms
+def generateTable():
+    res = "<div align=\"center\" style=\"width:50%%; height:50%%; float:%s\">\n" % (left_or_right())
+    terms = generateNumbers()
+    ts = terms
+    ts[12] = 'FREE SPACE'
+    res += "<table>\n"
+
+    bingo = "BINGO"
+
+    res += "\t<tr>\n"
+    for i in range(0, len(bingo)):
+        res += "\t\t<td><font size=\"24\">%s</font></td>\n" % (bingo[i])
+    res += "\t</tr>\n"
+
     for i, term in enumerate(ts):
         if i % 5 == 0:
             res += "\t<tr>\n"
@@ -41,17 +117,17 @@ def generateTable(terms, pagebreak = True):
         if i % 5 == 4:
             res += "\t</tr>\n"
     res += "</table>\n"
+    res += "</div>\n"
     return res
 
-out_file = open(sys.argv[2], 'w')
+out_file = open(sys.argv[1], 'w')
 out_file.write(head)
-cards = int(sys.argv[3])
+cards = int(sys.argv[2])
 for i in range(cards):
-    random.shuffle(terms)
-    if i != cards - 1:
-        out_file.write(generateTable(terms))
-    else:
-        out_file.write(generateTable(terms, False))
+    if i % 4 == 0 and i != 0:
+        out_file.write("<table class=\"newpage\"></table>")
+    out_file.write(generateTable())
+
 out_file.write("</body></html>")
 
 out_file.close()
